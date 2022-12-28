99a55391269b45550a69cb2189af366361e42f46
Wesley Silva <wesleyysilva@yahoo.com.br>
2022-12-27 22:03:03 -0300

    update

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    add(0, list)
+  end
+
+  defp add(count, list) when list != [] do
+    [_head | cauda] = list
+    add(count + 1, cauda)
+  end
+
+  defp add(count, []) do
+    count
   end
 end
